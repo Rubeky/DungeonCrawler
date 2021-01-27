7006eeed3f14edf041e4f10564f3b9f3c8978582
Rubeky <ruben.kelly@outlook.com>
2021-01-27 21:52:58 +1300

    Changed over to character with better licensing and less files

--- DungeonCrawler.py
+++ DungeonCrawler.py
@@ -3,15 +3,14 @@
 
 #Initialising pygame library
 import pygame
-import os
-
-print(os.getcwd())
+import random
 
 ###############################################################################
 ###############################################################################
 
 class Game:
-    walking = [[]]
+    walking = [[None]*4 for _ in range(4)]
+    tiles = [None]*4
 
 ###############################################################################
 
@@ -28,32 +27,54 @@ class Game:
         pygame.display.set_caption("Dungeon Crawler")
         screen.fill((255, 255, 255))
 
+        walking = [None]*4
+        walking[0] = pygame.image.load('Images/Up.png')
+        walking[1] = pygame.image.load('Images/Left.png')
+        walking[2] = pygame.image.load('Images/Down.png')
 
-        self.walking = [\
+        for i in range(4):
+            self.walking[0][i] = walking[0].subsurface((0 + i*16, 0, 16, 16))
+            self.walking[1][i] = walking[1].subsurface((0 + i*16, 0, 16, 16))
+            self.walking[2][i] = walking[2].subsurface((0 + i*16, 0, 16, 16))
+            self.walking[3][i] = pygame.transform.flip(self.walking[1][i], True, False)
+
+        '''self.walking = [\
         [pygame.image.load('Images/Up1.png'), pygame.image.load('Images/Up2.png'), pygame.image.load('Images/Up3.png'), pygame.image.load('Images/Up4.png')],\
         [pygame.image.load('Images/Left1.png'), pygame.image.load('Images/Left2.png'), pygame.image.load('Images/Left3.png'), pygame.image.load('Images/Left4.png')],\
         [pygame.image.load('Images/Down1.png'), pygame.image.load('Images/Down2.png'), pygame.image.load('Images/Down3.png'), pygame.image.load('Images/Down4.png')],\
         [pygame.image.load('Images/Right1.png'), pygame.image.load('Images/Right2.png'), pygame.image.load('Images/Right3.png'), pygame.image.load('Images/Right4.png')]\
-        ]
+        ]'''
 
+        tile_sheet = pygame.image.load('Images/Tiles.png')
+        #TODO: Choose better tiles
+        self.tiles[0] = tile_sheet.subsurface((17 * 17, 17 * 10, 16, 16))
+        self.tiles[1] = tile_sheet.subsurface((17 * 18, 17 * 10, 16, 16))
+        self.tiles[2] = tile_sheet.subsurface((17 * 10, 17 * 11, 16, 16))
+        self.tiles[3] = tile_sheet.subsurface((17 * 10, 17 * 12, 16, 16))
+
+
+        #Scaling all the tiles and assets loaded in
         for i in range(0,4):
+            self.tiles[i] = pygame.transform.scale(self.tiles[i], (64, 64))
             for j in range(0,4):
                 self.walking[i][j] = pygame.transform.scale(self.walking[i][j], (64, 64))
 
         running = True
         gameUpdated = False
-        tile_grid =  [["Empty"]*10 for _ in range(10)]
-        obstacle_grid = [["None"]*10 for _ in range(10)]
-
-        self.generateDungeon(tile_grid, obstacle_grid)
+        tile_grid =  [["None"]*16 for _ in range(16)]
+        obstacle_grid = [["None"]*16 for _ in range(16)]
 
         #Player attributes
         player_position = [160, 160] #Relative to top corner of board, in "pixels"
         player_health = 10
-        player_direction = ""
+        player_direction = -1
         player_score = 0
         items_list = ""
 
+        self.generateDungeon(tile_grid, obstacle_grid)
+        self.showScreen(player_position, player_direction, tile_grid, obstacle_grid, screen, player_health, items_list)
+
+        screen.blit(self.walking[2][0], [480, 480])
 
         pygame.display.update()
 
@@ -92,7 +113,7 @@ class Game:
 
             if gameUpdated:
                 #Populates screen
-                player_position, player_health = self.drawMovement(player_position, player_direction, tile_grid, obstacle_grid, screen, player_health, items_list)
+                player_position, player_health = self.showScreen(player_position, player_direction, tile_grid, obstacle_grid, screen, player_health, items_list)
                 player_direction = ""
 
 
@@ -118,8 +139,17 @@ class Game:
     '''
     def generateDungeon(self, tile_grid, obstacle_grid):
         #Randomise tile_grid to be a nice background
+        for i in range(0, 16):
+            for j in range(0, 16):
+                randomNumber = random.randint(0,3)
+                tile_grid[i][j] = self.tiles[randomNumber]
+
         #Generate obstacle_grid using premade tile sequences
-        pass
+        for x in range(0, 4):
+            for y in range(0, 4):
+                for i in range(0, 4):
+                    for j in range(0, 4):
+                        pass
 
 ###############################################################################
 
@@ -130,7 +160,7 @@ class Game:
 
      - This function draws the walking animation between blocks
     '''
-    def drawMovement(self, player_position, player_direction, tile_grid, obstacle_grid, screen, player_health, items_list):
+    def showScreen(self, player_position, player_direction, tile_grid, obstacle_grid, screen, player_health, items_list):
 
         response = self.playerMovementCheck(player_position, player_direction, obstacle_grid)
         if response == 3:
@@ -138,11 +168,21 @@ class Game:
         elif response == 2:
             player_health -= 1
 
-        for i in range(0, 16):
-            pygame.time.delay(100)
+        for i in range(0, 8):
+            pygame.time.delay(120)
             #Background
             screen.fill((255, 255, 255))
-            self.drawToScreen(player_position, tile_grid, obstacle_grid)
+
+            if player_direction == 0:
+                player_position[1] += 8
+            if player_direction == 1:
+                player_position[0] -= 8
+            if player_direction == 2:
+                player_position[1] -= 8
+            if player_direction == 3:
+                player_position[0] += 8
+
+            self.drawBackground(player_position, tile_grid, obstacle_grid, screen)
             #Character
             screen.blit(self.walking[player_direction][i%4], [480, 480])
             #UI
@@ -151,18 +191,35 @@ class Game:
             pygame.display.update()
 
 
+
         return player_position, player_health
 
 ###############################################################################
 
-    def drawToScreen(self, player_position, tile_grid, obstacle_grid):
+    def drawBackground(self, player_position, tile_grid, obstacle_grid, screen):
         #Find where the tiles start relative to the screen
-        tile_pos = [480 - player_position[0], 480 - player_position[1]]
+        tile_pos = [480 - 32 - player_position[0], 480 - player_position[1]]
         for i in range(0, 16):
-            for j in range(0,16):
-                #Draw tiles at position (tile_pos + 64*distance)
-                #Draw walls/objects
+            for j in range(0, 16):
+                screen.blit(tile_grid[i][j], ((tile_pos[0] + 64*i), ( - tile_pos[1] + 64*j)))
+                if tile_grid[i][j] == "None":
+                    pass
+                elif tile_grid[i][j] == "Tile1":
+                    pass
+                elif tile_grid[i][j] == "Tile2":
+                    pass
+                elif tile_grid[i][j] == "Tile3":
+                    pass
+                elif tile_grid[i][j] == "Tile4":
+                    pass
+                elif tile_grid[i][j] == "RareTile":
+                    pass
+                elif tile_grid[i][j] == "VeryRareTile":
+                    pass
 
+
+                #Draw walls/objects
+                pass
         pass
 
 ###############################################################################
@@ -190,13 +247,13 @@ class Game:
      4: Box to push ## TODO:
     '''
     def playerMovementCheck(self, player_position, player_direction, obstacle_grid):
-        #Check for "wall", "pit", "cracked wall",
+        #Check for "wall", "pit", "cracked wall", "box", ""
         pass
 
 ###############################################################################
 
     def breakBlock(self, player_position, player_direction, obstacle_grid):
-        #Check for "cracked_wall", break it
+        #Check for "cracked_wall" in front of player, break it
         return obstacle_grid
 
 ###############################################################################
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # DungeonCrawler
-This is a basic dungeon crawler based around the python module pygame. It was a project to help me learn the basics of pygame as well as develop my skills in python
+This is a basic dungeon crawler based around the python module pygame. It was a project to help me learn the basics of pygame as well as develop my skills in python.
+
+See Documentation.txt for details on planned features
+
+Credits:
+
+  Good documentation
+  https://realpython.com/pygame-a-primer/
+
+  Tech with Tim for good walkthrough of starting this off
+  https://www.youtube.com/watch?v=i6xMBig-pP4&list=PLzMcBGfZo4-lp3jAExUCewBfMx3UZFkh5
+
+  StackOverflow in general
+
+  Main character assets:
+  https://lvlupper.itch.io/16x16-rpg-character-base
+
+  Kenney Game Assets for most background stuff
+  https://kenney.itch.io/kenney-game-assets-1
+
+  
